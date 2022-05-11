d5461e837c457526ebb8ff688a21a0ac8f31c5fb
Rogelio Beristain <Rogelio26.dev@gmail.com>
2022-05-10 20:48:32 -0500

    refactor: clean code of de validators middlewares

---
+++ src/middleware/validators.middleware.js
@@ -0,0 +1,12 @@
+const { body, MinMaxOptions } = require('express-validator');
+/**
+ * 
+ * @param  {string} inputName is a String 
+ * @param {MinMaxOptions} MinMaxOptions of ValidatorExpress { min: number, max: number }
+ * @returns ExpressValidator
+ */
+ const isValidInputText = (inputName, MinMaxOptions)=> {
+    return body(inputName).isLength((MinMaxOptions)).not().isEmpty().trim().escape();
+ }
+
+ module.exports = { isValidInputText };
--- src/v1/routes/clientRoutes.js
+++ src/v1/routes/clientRoutes.js
@@ -1,38 +1,35 @@
 const express = require("express");
-const clientController = require("../../controllers/clientController");
+const {createNewClient, getInfoClient, apovateClient, unaprovateClient, getAllClients } = require("../../controllers/clientController");
+const {isValidInputText} = require("../../middleware/validators.middleware");
 const router = express.Router();
 var multer = require('multer');
-const { body } = require('express-validator');
+
+
+
 
 const upload = multer({ dest: 'public/data/uploads' });
 
 router.post("/",
     //middlewares
     upload.array('filesClient'),
-    isValidInputText('name',3),
-    isValidInputText('firstSurname',3),
-    isValidInputText('secondSurname',3),
-    isValidInputText('zipCodeAddress',1),
-    isValidInputText('streetAddress',1),
-    isValidInputText('numberAddress',1),
-    isValidInputText('colonyAddress',1),
-    body('phoneNumber').isLength(({ min: 10, max:10 })),
-    body('rfc').isLength(({ min: 9 })),
+    isValidInputText('name', { min: 3 }),
+    isValidInputText('firstSurname', { min: 3 }),
+    isValidInputText('secondSurname', { min: 3 }),
+    isValidInputText('zipCodeAddress', { min: 1 }),
+    isValidInputText('streetAddress', { min: 1 }),
+    isValidInputText('numberAddress', { min: 1 }),
+    isValidInputText('colonyAddress', { min: 1 }),
+    isValidInputText('phoneNumber',{ min: 10, max: 10 }),
+    isValidInputText('rfc', { min: 9 }),
     //EndMiddleswares
-    clientController.createNewClient);
+    createNewClient);
 
-router.get("/info/:clientId", clientController.getInfoClient);
+router.get("/info/:clientId", getInfoClient);
 
-router.patch("aprovate/:clientId", clientController.apovateClient);
+router.patch("aprovate/:clientId", apovateClient);
 
-router.patch("unaprovate/:clientId", clientController.unaprovateClient);
+router.patch("unaprovate/:clientId", unaprovateClient);
 
-router.get("/", clientController.getAllClients); 
-
-function isValidInputText(inputName,min = null, max = null) {
-    if(min && max) return body(inputName).isLength(({ min: min, max: max })).not().isEmpty().trim().escape();
-    else if (min) return body(inputName).isLength(({ min: min })).not().isEmpty().trim().escape();
-    else if (max)return body(inputName).isLength(({ max: max })).not().isEmpty().trim().escape();
-}
+router.get("/", getAllClients); 
 
 module.exports = router;
