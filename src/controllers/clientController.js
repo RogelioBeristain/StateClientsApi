5c547154acd4d6e805f2226141551bce50caea4d
Rogelio Beristain <Rogelio26.dev@gmail.com>
2022-05-10 19:57:20 -0500

    feat:  middlewares were implemented for inputs

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,28 +1,35 @@
 const clientService = require("../services/clientService");
+const { validationResult } = require('express-validator');
 
 const createNewClient = (req, res) => {
   const createdclient = clientService.createNewClient();
-  res.send("Create a new client");
+
+  const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send({message:"createNewClient", data:req.body });
 };
 
 const getInfoClient = (req, res) => {
   const createdclient = clientService.getInfoClient();
-  res.send("Create a new client");
+  res.send({message:"getInfoClient"});
 };
 
 const apovateClient = (req, res) => {
   const createdclient = clientService.apovateClient();
-  res.send("Create a new client");
+  res.send({message:"apovateClient"});
 };
 
 const unaprovateClient = (req, res) => {
   const createdclient = clientService.unaprovateClient();
-  res.send("Create a new client");
+  res.send({message:"unaprovateClient"});
 };
 
 const getAllClients = (req, res) => {
   const createdclient = clientService.getAllClients();
-  res.send("Create a new client");
+  res.send({message:"getAllClients"});
 };
 
 module.exports = {
--- src/v1/routes/clientRoutes.js
+++ src/v1/routes/clientRoutes.js
@@ -2,10 +2,24 @@ const express = require("express");
 const clientController = require("../../controllers/clientController");
 const router = express.Router();
 var multer = require('multer');
+const { body } = require('express-validator');
 
 const upload = multer({ dest: 'public/data/uploads' });
 
-router.post("/", upload.array('userphoto'), clientController.createNewClient);
+router.post("/",
+    //middlewares
+    upload.array('filesClient'),
+    isValidInputText('name',3),
+    isValidInputText('firstSurname',3),
+    isValidInputText('secondSurname',3),
+    isValidInputText('zipCodeAddress',1),
+    isValidInputText('streetAddress',1),
+    isValidInputText('numberAddress',1),
+    isValidInputText('colonyAddress',1),
+    body('phoneNumber').isLength(({ min: 10, max:10 })),
+    body('rfc').isLength(({ min: 9 })),
+    //EndMiddleswares
+    clientController.createNewClient);
 
 router.get("/info/:clientId", clientController.getInfoClient);
 
@@ -14,5 +28,11 @@ router.patch("aprovate/:clientId", clientController.apovateClient);
 router.patch("unaprovate/:clientId", clientController.unaprovateClient);
 
 router.get("/", clientController.getAllClients); 
+
+function isValidInputText(inputName,min = null, max = null) {
+    if(min && max) return body(inputName).isLength(({ min: min, max: max })).not().isEmpty().trim().escape();
+    else if (min) return body(inputName).isLength(({ min: min })).not().isEmpty().trim().escape();
+    else if (max)return body(inputName).isLength(({ max: max })).not().isEmpty().trim().escape();
+}
 
 module.exports = router;
